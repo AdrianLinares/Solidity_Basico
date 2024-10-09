9fd6982e7458c727511a05be587240853e672a77
AdrianLinares <adrianlinares246@gmail.com>
2024-10-08 19:20:10 -0500

    primer contrato

---
+++ hola_mundo.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+//0x43F6b63347dA74e03B321A76E86B134dAD8E4228=
+
+pragma solidity 0.8.24; // versión del compilador
+
+// se recomienda que el nombre del contrato sea el mismo que el del archivo
+contract hola_mundo {
+    // declarando mi variable saludo
+    string public saludo = "Hola desde EducaTeth 2.0";
+
+}
+
+
