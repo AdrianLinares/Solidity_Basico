496d646cc8ae38a87a700e954fa227620bba6187
AdrianLinares <adrianlinares246@gmail.com>
2024-10-27 17:28:58 -0500

    contrato erc20

---
+++ ERC20.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+/// @custom:security-contact jalinaresm@ut.edu.co
+contract HeavyMetalToken is ERC20, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+        ERC20("Heavy Metal Token", "HMT")
+        Ownable(initialOwner)
+        ERC20Permit("Heavy Metal Token")
+    {
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
